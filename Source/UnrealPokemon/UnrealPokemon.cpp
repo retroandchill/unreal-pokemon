4b0743ffb451926e4ee5fdaab09047f96859a7ff
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-01-24 19:00:18 -0500

    Created the initial Unreal project

---
+++ Source/UnrealPokemon/UnrealPokemon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPokemon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPokemon, "UnrealPokemon" );
