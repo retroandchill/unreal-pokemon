039fb84e601e184ce9290d6b0bbe14fa180e88b2
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-02-05 17:14:11 -0500

    Added a class to test importing data from methods

---
+++ Content/Python/src/pokemon/data_loader/unreal_data_loader.py
@@ -0,0 +1,13 @@
+from typing import Callable, Optional
+
+
+class UnrealDataLoader:
+    def __init__(self, getter: Callable[[], set[str]]):
+        self.__getter = getter
+        self.__data: Optional[set[str]] = None
+
+    def __contains__(self, item: str) -> bool:
+        if self.__data is None:
+            self.__data = self.__getter()
+
+        return item in self.__data
