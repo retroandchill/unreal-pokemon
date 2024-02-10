54b49b4807655b6d1e238a7a4e9f94560c328fc8
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-02-10 13:15:03 -0500

    Added a general static registry object

---
+++ Source/PokemonUtilities/Public/Meta/GenericStaticRegistry.h
@@ -0,0 +1,26 @@
+// Created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PokeRegistry.h"
+
+/**
+ * The generic version of the static registry that requires no additional specialized methods, and can act as a
+ * singleton object all on its own
+ */
+template <typename T, typename ...Args>
+class TGenericStaticRegistry : public TPokeRegistry<T, Args...> {
+	TGenericStaticRegistry() = default;
+	~TGenericStaticRegistry() = default;
+
+public:
+	/**
+	 * Get the singleton instance of the class
+	 * @return A reference to the only instance of this class
+	 */
+	static TGenericStaticRegistry& GetInstance() {
+		static TGenericStaticRegistry Instance;
+		return Instance;
+	}
+};
