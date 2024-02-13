f9b363daf9afe81cc8f24237277405b03f19501f
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-02-13 16:33:12 -0500

    Created the Map class

---
+++ Source/GridBased2D/Private/Map/GridBasedMap.cpp
@@ -0,0 +1,33 @@
+//====================================================================================================================
+// ** Unreal Pokémon created by Retro & Chill
+//--------------------------------------------------------------------------------------------------------------------
+// This project is intended as a means of learning more about how a game like Pokémon works by creating a framework
+// from the ground up, and for non-commercial applications. While this code is original, Pokémon is the intellectual
+// property of Game Freak and Nintendo, as such it is highly discouraged to use this kit to make a commercial product.
+//--------------------------------------------------------------------------------------------------------------------
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//====================================================================================================================
+#include "Map/GridBasedMap.h"
+
+
+// Sets default values
+AGridBasedMap::AGridBasedMap() {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGridBasedMap::BeginPlay() {
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGridBasedMap::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
