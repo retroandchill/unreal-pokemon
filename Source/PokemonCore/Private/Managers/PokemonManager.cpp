238280384c7da4d316ecdf0e202ccd6a3b827bf1
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-03-02 15:25:47 -0500

    Created dummy trainer class

---
+++ Source/PokemonCore/Private/Managers/PokemonManager.cpp
@@ -0,0 +1,23 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+#include "Managers/PokemonManager.h"
+
+#include "Trainers/TrainerStub.h"
+
+// TODO: Swap this instantiation with the actual trainer instantiation
+FPokemonManager::FPokemonManager() : Player(MakeUnique<FTrainerStub>()) {
+}
+
+FPokemonManager::~FPokemonManager() = default;
+
+FPokemonManager& FPokemonManager::GetInstance() {
+	static FPokemonManager Instance;
+	return Instance;
+}
+
+ITrainer& FPokemonManager::GetPlayer() {
+	return *Player;
+}
+
+const ITrainer& FPokemonManager::GetPlayer() const {
+	return *Player;
+}
---
+++ Source/PokemonCore/Private/Trainers/TrainerStub.cpp
@@ -0,0 +1,25 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Trainers/TrainerStub.h"
+
+#include "Pokemon/GamePokemon.h"
+
+
+FTrainerStub::FTrainerStub() {
+	// Create the basic test party for now
+	Party.Add(MakeShared<FGamePokemon>("PIKACHU", 10));
+	Party.Add(MakeShared<FGamePokemon>("SNIVY", 10));
+	Party.Add(MakeShared<FGamePokemon>("TEPIG", 10));
+	Party.Add(MakeShared<FGamePokemon>("OSHAWOTT", 10));
+	Party.Add(MakeShared<FGamePokemon>("AXEW", 10));
+	Party.Add(MakeShared<FGamePokemon>("RIOLU", 10));
+}
+
+const TArray<TSharedRef<IPokemon>>& FTrainerStub::GetParty() {
+	return Party;
+}
+
+const TArray<TSharedRef<const IPokemon>>& FTrainerStub::GetParty() const {
+	return static_cast<TArray<TSharedRef<const IPokemon>>>(Party);
+}
