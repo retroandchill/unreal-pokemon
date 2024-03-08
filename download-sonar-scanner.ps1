e0ee2bcbc7da3681f13e780c0096076e2d2dfc76
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-03-07 22:22:01 -0500

    Set the Sonar variables each step

--- download-sonar-scanner.ps1
+++ download-sonar-scanner.ps1
@@ -1,6 +1,4 @@
-﻿$env:SONAR_SCANNER_VERSION = "5.0.1.3006"
-$env:SONAR_DIRECTORY = [System.IO.Path]::Combine($(get-location).Path,".sonar")
-$env:SONAR_SCANNER_HOME = "$env:SONAR_DIRECTORY/sonar-scanner-$env:SONAR_SCANNER_VERSION-windows"
+﻿./set-sonar-variables.ps1
 rm $env:SONAR_SCANNER_HOME -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -path $env:SONAR_SCANNER_HOME -type directory
 (New-Object System.Net.WebClient).DownloadFile("https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$env:SONAR_SCANNER_VERSION-windows.zip", "$env:SONAR_DIRECTORY/sonar-scanner.zip")
