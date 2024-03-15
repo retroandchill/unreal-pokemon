196001b79468fe1e15373c5f0a1e25770682f825
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-03-15 15:13:32 -0400

    Set up the ability for nodes to have a drop down

---
+++ Source/PokemonData/PokemonDataDeveloper/Private/Pins/PokemonDataGraphPinFactory.cpp
@@ -0,0 +1,26 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Pins/PokemonDataGraphPinFactory.h"
+
+#include "SGraphPinNameList.h"
+#include "Nodes/SelectDataRow.h"
+
+TSharedPtr<SGraphPin> FPokemonDataGraphPinFactory::CreatePin(UEdGraphPin* Pin) const {
+	auto* Outer = Cast<ISelectDataRow>(Pin->GetOuter());
+	if (Pin->PinType.PinCategory != UEdGraphSchema_K2::PC_Name || Outer == nullptr || Pin != Outer->GetRowPin()) {
+		return FGraphPanelPinFactory::CreatePin(Pin);
+	}
+
+	auto RowNames = Outer->GetRowNames();
+	if (RowNames.IsEmpty()) {
+		return FGraphPanelPinFactory::CreatePin(Pin);
+	}
+
+	TArray<TSharedPtr<FName>> NamesList;
+	for (auto Name : RowNames) {
+		NamesList.Add(MakeShared<FName>(Name));
+	}
+	
+	return SNew(SGraphPinNameList, Pin, NamesList);
+}
---
+++ Source/PokemonData/PokemonDataDeveloper/Public/Pins/PokemonDataGraphPinFactory.h
@@ -0,0 +1,15 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlueprintGraphPanelPinFactory.h"
+#include "EdGraphUtilities.h"
+
+/**
+ * Factory class to handling the visuals of pins for the Pokémon Data module
+ */
+class POKEMONDATADEVELOPER_API FPokemonDataGraphPinFactory : public FGraphPanelPinFactory {
+public:
+  TSharedPtr<SGraphPin> CreatePin(UEdGraphPin* Pin) const override;
+};
