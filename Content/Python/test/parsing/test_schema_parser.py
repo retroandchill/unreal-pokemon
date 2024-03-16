88b6adb87a4f1a1b62c9c069cda8a019fefb09d6
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-03-16 15:51:56 -0400

    Added missing files

---
+++ Content/Python/test/parsing/test_schema_parser.py
@@ -0,0 +1,40 @@
+import unittest
+
+from pokemon.data_loader.schema_parser import string_to_json_value
+
+
+class MyTestCase(unittest.TestCase):
+    def test_string_to_json_value(self):
+        self.assertEqual(string_to_json_value('2', 'i'), 2)
+
+        self.assertEqual(string_to_json_value('4', 'u'), 4)
+        with self.assertRaises(ValueError):
+            string_to_json_value('-3', 'u')
+
+        self.assertEqual(string_to_json_value('8', 'v'), 8)
+        with self.assertRaises(ValueError):
+            string_to_json_value('-3', 'v')
+        with self.assertRaises(ValueError):
+            string_to_json_value('0', 'v')
+
+        self.assertEqual(string_to_json_value('2a', 'x'), 0x2a)
+        self.assertEqual(string_to_json_value('4.3', 'f'), 4.3)
+        self.assertTrue(string_to_json_value('True', 'b'))
+
+        self.assertEqual(string_to_json_value('field_name3', 'n'), 'field_name3')
+        with self.assertRaises(ValueError):
+            string_to_json_value('$invalid_name', 'n')
+        self.assertEqual(string_to_json_value('field_name3', 's'), 'field_name3')
+        self.assertEqual(string_to_json_value('field_name3', 'q'), 'field_name3')
+        self.assertEqual(string_to_json_value('field_name3', 'm'), 'field_name3')
+        self.assertEqual(string_to_json_value('EnumValue', 'e'), 'EnumValue')
+        self.assertEqual(string_to_json_value('EnumValue', 'e', ['EnumValue', 'Test']), 'EnumValue')
+        with self.assertRaises(ValueError):
+            string_to_json_value('EnumValue', 'e', ['Test'])
+
+        self.assertEqual(string_to_json_value('-3', 'y'), -3)
+        self.assertEqual(string_to_json_value('EnumValue', 'y', ['EnumValue', 'Test']), 'EnumValue')
+
+
+if __name__ == '__main__':
+    unittest.main()
