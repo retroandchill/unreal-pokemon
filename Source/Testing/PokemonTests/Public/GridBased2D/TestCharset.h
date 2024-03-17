c3ad4d9b499d0fbffa9ba70ba91dd2b8bb7c20ff
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-03-17 14:31:36 -0400

    Added a test for the charset

---
+++ Source/Testing/PokemonTests/Public/GridBased2D/TestCharset.h
@@ -0,0 +1,35 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FunctionalTest.h"
+#include "TestCharset.generated.h"
+
+class UCharset;
+/**
+ * Test class for a charset
+ */
+UCLASS()
+class POKEMONTESTS_API ATestCharset : public AFunctionalTest {
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Sets default values for this actor's properties
+	 */
+	ATestCharset();
+
+private:
+	/**
+	 * Actually run the test
+	 */
+	UFUNCTION()
+	void RunTest();
+
+	/**
+	 * The Windowskin asset to verify what to do when there is a valid texture
+	 */
+	UPROPERTY(EditInstanceOnly, Category = "Test Parameters")
+	TObjectPtr<UCharset> Charset;
+};
