739f9fa6666fee25efc384e2f96e228953d074a7
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-03-19 20:41:13 -0400

    Restored default game module

---
+++ Source/UnrealPokemon/UnrealPokemon.Build.cs
@@ -0,0 +1,22 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+using UnrealBuildTool;
+
+public class UnrealPokemon : ModuleRules
+{
+	public UnrealPokemon(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
