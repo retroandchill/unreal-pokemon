45763af8264618cffd39228a45a5c054577ef39e
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-03-31 21:34:25 -0400

    Added trainer type metadata into the game

--- Content/Python/test/parsing/test_unreal_data_loader.py
+++ Content/Python/test/parsing/test_unreal_data_loader.py
@@ -1,16 +1,4 @@
-# ====================================================================================================================
-# ** Unreal Pokémon created by Retro & Chill
-# --------------------------------------------------------------------------------------------------------------------
-# This project is intended as a means of learning more about how a game like Pokémon works by creating a framework
-# from the ground up, and for non-commercial applications. While this code is original, Pokémon is the intellectual
-# property of Game Freak and Nintendo, as such it is highly discouraged to use this kit to make a commercial product.
-# --------------------------------------------------------------------------------------------------------------------
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# ====================================================================================================================
+# "Unreal Pokémon" created by Retro & Chill.
 import unittest
 from unittest.mock import MagicMock
 
