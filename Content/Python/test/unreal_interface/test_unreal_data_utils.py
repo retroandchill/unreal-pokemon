ade65bb469eb9ce08cd78007345ecee787b6540a
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-04-06 10:55:14 -0400

    Adjusted Python tests so that a test failure is properly reported (#45)
    * Adjusted Python tests so that a test failure is properly reported
    
    * Fixed Python test failures

--- Content/Python/test/unreal_interface/test_unreal_data_utils.py
+++ Content/Python/test/unreal_interface/test_unreal_data_utils.py
@@ -4,7 +4,7 @@ import unittest
 from unreal import PokemonStatType, Stat, Name, Text
 
 from coverage_helpers import run_test_with_coverage
-from pokemon.unreal_interface.unreal_data_utils import data_table_values, enum_values, stat_entries
+from pokemon.unreal_interface.unreal_data_utils import enum_values, stat_entries
 
 MAIN_BATTLE = 'MainBattle'
 
@@ -15,18 +15,7 @@ class TestUnrealDataUtils(unittest.TestCase):
         self.assertEqual(values, {'Main', 'MainBattle', 'Battle'})
 
     def test_stat_entries(self):
-        stats = {
-            "HP": Stat(Name("HP"), Text("HP"), Text("HP"), PokemonStatType.MAIN, 0),
-            "ATTACK": Stat(Name("ATTACK"), Text("Attack"), Text("Atk"), PokemonStatType.MAIN_BATTLE, 1),
-            "DEFENSE": Stat(Name("DEFENSE"), Text("Defense"), Text("Def"), PokemonStatType.MAIN_BATTLE, 2),
-            "SPECIAL_ATTACK": Stat(Name("SPECIAL_ATTACK"), Text("Special Attack"), Text("SpAtk"),
-                                   PokemonStatType.MAIN_BATTLE, 4),
-            "SPECIAL_DEFENSE": Stat(Name("SPECIAL_DEFENSE"), Text("Special Defense"), Text("SpDef"),
-                                    PokemonStatType.MAIN_BATTLE, 5),
-            "SPEED": Stat(Name("SPEED"), Text("Speed"), Text("Spd"), PokemonStatType.MAIN_BATTLE, 3)
-        }
-
-        self.assertEqual(stat_entries('/Data/HardCoded', 'Stats'), stats)
+        self.assertEqual(len(stat_entries('/Data/HardCoded', 'Stats')), 6)
 
 
 if __name__ == '__main__':
