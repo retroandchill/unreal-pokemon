80c042e04e90f5d2bca2cf31f3296d31df0aa233
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-04-11 22:37:30 -0400

    Fixed issues with test coverage

--- Plugins/RPGMenus/Source/RPGMenus/Public/Primatives/DisplayTextOption.h
+++ Plugins/RPGMenus/Source/RPGMenus/Public/Primatives/DisplayTextOption.h
@@ -15,57 +15,23 @@ class RPGMENUS_API UDisplayTextOption : public USelectableOption {
 	GENERATED_BODY()
 	
 public:
-	/**
-	 * Get the display text as shown to the player
-	 * @return The display text
-	 */
-	UFUNCTION(BlueprintPure, Category = Text)
-	FText GetText() const;
-
-	/**
-	 * Set the display text for this widget
-	 * @param NewText The new display text for the widget
-	 */
+    /**
+     * Set the display text for this widget
+     * @param NewText The new display text for the widget
+     */
 	UFUNCTION(BlueprintCallable, Category = Display)
 	void SetText(const FText& NewText);
 
-	/**
-	 * Get the font to be displayed to the player
-	 * @return The font to set for the window
-	 */
-	UFUNCTION(BlueprintPure, Category = Display, meta = (BlueprintInternalUseOnly="true"))
-	const FSlateFontInfo& GetDisplayFont() const;
-
-	/**
-	 * Set the color of the text to the given new color
-	 * @param Color The new color for the text
-	 */
-	UFUNCTION(BlueprintCallable, Category = Display, meta = (BlueprintInternalUseOnly="true"))
-	void SetTextColor(const FSlateColor& Color);
-
-	/**
-	 * Get the size of the current text contained within this widget
-	 * @return The size of the current text
-	 */
+    /**
+     * Get the size of the current text contained within this widget
+     * @return The size of the current text
+     */
 	FVector2D GetTextSize() const;
 
-	/**
-	 * Get the size of the given text when displayed to the player
-	 * @param Text The text to display to the player
-	 * @return The size of the given text
-	 */
-	FVector2D GetTextSize(const FString& Text) const;
-
-	/**
-	 * Get the total size of the text drawing area
-	 * @return The total size of the area to draw the text in
-	 */
-	FVector2D GetTotalTextAreaSize() const;
-
-	/**
-	 * Get the padding around the display text for the player
-	 * @return The padding around the text
-	 */
+    /**
+     * Get the padding around the display text for the player
+     * @return The padding around the text
+     */
 	UFUNCTION(BlueprintCallable, Category = "Text|Size")
 	FMargin GetDisplayTextPadding() const;
 
--- Source/Testing/PokemonTests/Private/RPGMenus/SelectableWindowInputTest.cpp
+++ Source/Testing/PokemonTests/Private/RPGMenus/SelectableWindowInputTest.cpp
@@ -33,7 +33,11 @@ void ASelectableWindowInputTest::TestRun() {
     TEST_ASSERT(AssertEqual_Int(NewWidget->GetItemCount(), 6, TEXT("Widget should have 6 items!")));
     TEST_ASSERT(AssertEqual_Int(NewWidget->GetRowCount(), 3, TEXT("Widget should have 3 rows!")));
     TEST_ASSERT(AssertEqual_Int(NewWidget->GetColumnCount(), 2, TEXT("Widget should have 2 columns!")));
-
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetColumn(4), 1, TEXT("Column for Index 4")))
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetRow(4), 1, TEXT("Row for Index 4")))
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetColumn(5), 2, TEXT("Column for Index 5")))
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetRow(5), 1, TEXT("Row for Index 5")))
+    
     UInputUtilities::SimulateKeyPress(NewWidget, DownKey);
     TEST_ASSERT(AssertEqual_Int(NewWidget->GetIndex(), 2, TEXT("Index should be 2!")));
 
@@ -72,10 +76,19 @@ void ASelectableWindowInputTest::TestRun() {
     TEST_ASSERT(AssertTrue(NewIndex.IsSet(), TEXT("Confirm should be called!")))
     TEST_ASSERT(AssertEqual_Int(5, NewIndex.GetValue(), TEXT("Index should be 5!")));
 
+    NewWidget->SetActive(true);
+    TEST_ASSERT(AssertTrue(NewWidget->IsActive(), TEXT("If the widget is active we shouldn't change states.")))
+    
     NewWidget->OnCancel.AddDynamic(this, &ASelectableWindowInputTest::OnCancel);
     UInputUtilities::SimulateKeyPress(NewWidget, CancelKey);
     TEST_ASSERT(AssertTrue(bCanceled, TEXT("Cancel should be called!")))
 
+    NewWidget->Deselect();
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetIndex(), -1, TEXT("Deselection should be index -1")))
+    
+    NewWidget->Deselect();
+    TEST_ASSERT(AssertEqual_Int(NewWidget->GetIndex(), -1, TEXT("Deselection should be index -1")))
+    
     FinishTest(EFunctionalTestResult::Succeeded, TEXT("Test passed!"));
 }
 
