8201ec3ec57b984587c9b2670954e80ccf6153bc
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-05-11 12:26:52 -0400

    Added documentation

--- Plugins/PokemonUtilities/Source/PokemonUtilities/Public/Mainpulation/RangeHelpers.h
+++ Plugins/PokemonUtilities/Source/PokemonUtilities/Public/Mainpulation/RangeHelpers.h
@@ -20,6 +20,13 @@ namespace RangeHelpers {
         return std::views::all(Span);
     }
 
+    /**
+     * Convert the elements in the range into an array
+     * @tparam T The type of data the array will hold
+     * @tparam RangeType The input range type
+     * @param Range The range to process
+     * @return The range values as an array
+     */
     template <typename T, typename RangeType>
     TArray<T> ToArray(RangeType Range) {
         TArray<T> Ret;
@@ -29,9 +36,20 @@ namespace RangeHelpers {
         return Ret;
     }
 
+    /**
+     * Converter object used to transform a range into an array.
+     * @tparam T The type of data the array will hold
+     */
     template <typename T>
     struct TToArray {};
 
+    /**
+     * Operator overload so that converting to an Unreal data type can be chained into the ranges stream operator
+     * @tparam T The type of data the array will hold
+     * @tparam RangeType The input range type
+     * @param Range The range to process
+     * @return The range values as an array
+     */
     template <typename T, typename RangeType>
     TArray<T> operator|(RangeType Range, TToArray<T>) {
         return ToArray<T, RangeType>(Range);
