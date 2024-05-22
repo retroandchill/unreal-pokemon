829c9012698312d4ad3f4225ce303150b24313e6
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-05-21 20:47:05 -0400

    Created the Pokémon moves page

---
+++ Plugins/PokemonUI/Source/PokemonUI/Private/Components/Summary/PokemonMovesPage.cpp
@@ -0,0 +1,13 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Components/Summary/PokemonMovesPage.h"
+
+void UPokemonMovesPage::RefreshInfo_Implementation(const TScriptInterface<IPokemon> &Pokemon) {
+    Super::RefreshInfo_Implementation(Pokemon);
+    DrawPokemonMoves.Broadcast(Pokemon);
+}
+
+bool UPokemonMovesPage::CanSelectOnPage_Implementation() const {
+    return true;
+}
---
+++ Plugins/PokemonUI/Source/PokemonUI/Public/Components/Summary/PokemonMovesPage.h
@@ -0,0 +1,29 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SummaryScreenPage.h"
+#include "PokemonMovesPage.generated.h"
+
+/**
+ * Delegate for the drawing of a Pokémon's moves.
+ */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDrawPokemonMoves, const TScriptInterface<IPokemon>&, Pokemon);
+
+/**
+ * The page in the summary screen that displays the moves.
+ */
+UCLASS(Abstract)
+class POKEMONUI_API UPokemonMovesPage : public USummaryScreenPage {
+    GENERATED_BODY()
+
+protected:
+    void RefreshInfo_Implementation(const TScriptInterface<IPokemon> &Pokemon) override;
+    bool CanSelectOnPage_Implementation() const override;
+
+private:
+    UPROPERTY(BlueprintAssignable, Category = Events)
+    FDrawPokemonMoves DrawPokemonMoves;
+
+};
