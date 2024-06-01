7c9ae621e8837e0e740703469a0efeae3acbcc57
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-01 12:16:17 -0400

    Added a custom details panel customization for the dependency injection settings (#75)
    * Updated the details panel so that injection targets are restricted to their interfaces
    
    * Got a test for the dependency injection settings widget code
    
    * Addressed SonarCloud issues
    
    * Applied Clang-Formatting

---
+++ Plugins/UnrealInjector/Source/UnrealInjectorEditor/Private/Details/InjectionTargetCustomization.cpp
@@ -0,0 +1,26 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#include "Details/InjectionTargetCustomization.h"
+#include "DetailWidgetRow.h"
+
+TSharedRef<IPropertyTypeCustomization> FInjectionTargetCustomization::MakeInstance() {
+    return MakeShared<FInjectionTargetCustomization>();
+}
+
+void FInjectionTargetCustomization::CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle,
+                                                    FDetailWidgetRow &HeaderRow,
+                                                    IPropertyTypeCustomizationUtils &CustomizationUtils) {
+    UObject *InterfaceObject;
+    PropertyHandle->GetChildHandle(TEXT("TargetInterface"))->GetValue(InterfaceObject);
+    auto WrappedProperty = PropertyHandle->GetChildHandle(TEXT("InjectedClass"));
+    WrappedProperty->GetProperty()->SetMetaData(TEXT("MustImplement"), InterfaceObject->GetName());
+    HeaderRow.NameContent()[PropertyHandle->CreatePropertyNameWidget()]
+        .ValueContent()[WrappedProperty->CreatePropertyValueWidget(false)];
+    WrappedProperty->GetProperty()->RemoveMetaData(TEXT("MustImplement"));
+}
+
+void FInjectionTargetCustomization::CustomizeChildren(TSharedRef<IPropertyHandle> PropertyHandle,
+                                                      IDetailChildrenBuilder &ChildBuilder,
+                                                      IPropertyTypeCustomizationUtils &CustomizationUtils) {
+    // No child customization
+}
---
+++ Plugins/UnrealInjector/Source/UnrealInjectorEditor/Public/Details/InjectionTargetCustomization.h
@@ -0,0 +1,18 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ *
+ */
+class UNREALINJECTOREDITOR_API FInjectionTargetCustomization : public IPropertyTypeCustomization {
+  public:
+    static TSharedRef<IPropertyTypeCustomization> MakeInstance();
+
+    void CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow &HeaderRow,
+                         IPropertyTypeCustomizationUtils &CustomizationUtils) override;
+    void CustomizeChildren(TSharedRef<IPropertyHandle> PropertyHandle, IDetailChildrenBuilder &ChildBuilder,
+                           IPropertyTypeCustomizationUtils &CustomizationUtils) override;
+};
