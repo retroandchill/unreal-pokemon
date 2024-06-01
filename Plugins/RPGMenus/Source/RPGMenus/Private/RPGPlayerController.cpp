8119907e85fb00dd2aa8683a35c72f01ff625ca6
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-01 12:27:53 -0400

    Actually removed the class

--- Plugins/RPGMenus/Source/RPGMenus/Private/RPGPlayerController.cpp
+++
@@ -1,4 +0,0 @@
-﻿// "Unreal Pokémon" created by Retro & Chill.
-#include "RPGPlayerController.h"
-#include "Blueprint/UserWidget.h"
-#include "Screens/Screen.h"
--- Plugins/RPGMenus/Source/RPGMenus/Public/RPGPlayerController.h
+++
@@ -1,9 +0,0 @@
-﻿// "Unreal Pokémon" created by Retro & Chill.
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "GameFramework/PlayerController.h"
-#include "Screens/Screen.h"
-
-#include "RPGPlayerController.generated.h"
