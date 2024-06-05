a5f723a7c096630f7b249a27124a183d686c17e9
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-04 22:18:24 -0400

    Finalized the object being used now

--- Plugins/PokemonCore/Source/PokemonCore/Private/Utilities/TrainerHelpers.cpp
+++ Plugins/PokemonCore/Source/PokemonCore/Private/Utilities/TrainerHelpers.cpp
@@ -4,10 +4,10 @@
 #include "Kismet/GameplayStatics.h"
 #include "Managers/PokemonSubsystem.h"
 
-const TScriptInterface<ITrainer> &UTrainerHelpers::GetPlayerCharacter(const UObject *WorldContext) {
-    return UGameplayStatics::GetGameInstance(WorldContext)->GetSubsystem<UPokemonSubsystem>()->GetPlayer();
+const TScriptInterface<ITrainer> &UTrainerHelpers::GetPlayerCharacter(const UObject *WorldContextObject) {
+    return UGameplayStatics::GetGameInstance(WorldContextObject)->GetSubsystem<UPokemonSubsystem>()->GetPlayer();
 }
 
-const TScriptInterface<IBag> & UTrainerHelpers::GetBag(const UObject *WorldContext) {
-    return UGameplayStatics::GetGameInstance(WorldContext)->GetSubsystem<UPokemonSubsystem>()->GetBag();
+const TScriptInterface<IBag> & UTrainerHelpers::GetBag(const UObject *WorldContextObject) {
+    return UGameplayStatics::GetGameInstance(WorldContextObject)->GetSubsystem<UPokemonSubsystem>()->GetBag();
 }
