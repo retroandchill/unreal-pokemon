76f9381adb67ffcfabac4e6ac874a9a3bae24070
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-08 10:40:30 -0400

    Created the battle module

---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Public/PokemonBattle.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+/**
+ * Module that handles all battle related code
+ */
+class FPokemonBattleModule : public IModuleInterface {
+public:
+    void StartupModule() override;
+    void ShutdownModule() override;
+};
