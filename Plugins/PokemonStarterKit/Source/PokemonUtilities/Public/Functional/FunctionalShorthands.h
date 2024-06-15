8daf49cf8dffc7dd23ef17ee5ae75fb137c630a5
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-15 11:51:59 -0400

    Fixed child widget search

--- Plugins/PokemonStarterKit/Source/PokemonUtilities/Public/Functional/FunctionalShorthands.h
+++ Plugins/PokemonStarterKit/Source/PokemonUtilities/Public/Functional/FunctionalShorthands.h
@@ -6,6 +6,7 @@
 
 /**
  * Shorthand for a single argument callback that takes in an interface
+ * @tparam T The interface type we're looking for
  */
 template <typename T>
 using TInterfaceCallback = const TFunctionRef<void(const TScriptInterface<T>&)>&;
