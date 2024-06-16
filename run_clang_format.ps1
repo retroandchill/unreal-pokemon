2a9b2674c3432fc893c60b01ccb51ac0801b0df7
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-16 12:00:22 -0400

    Added a git reset hard for submodules to clang-format

--- run_clang_format.ps1
+++ run_clang_format.ps1
@@ -1,3 +1,5 @@
 ﻿Get-ChildItem -Recurse . -Include *.cpp, *.h -Exclude "**/Deps/**" -ErrorAction SilentlyContinue -Force | ForEach-Object { 
     clang-format -i $_ 
 }
+
+git submodule foreach --recursive git reset --hard
