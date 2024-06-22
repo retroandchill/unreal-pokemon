c85f2f0cf4f9f58b4c318b59ca2853ebc2818ff1
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-21 20:30:40 -0400

    Setup dynamic class lookup for moves

---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Private/Battle/Moves/MoveLookup.cpp
@@ -0,0 +1,22 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Battle/Moves/MoveLookup.h"
+#include "Battle/Moves/BaseBattleMove.h"
+#include "Battle/Moves/BattleMove.h"
+#include "Settings/BaseSettings.h"
+
+
+UClass * Battle::Moves::LookupMoveEffectClass(FName FunctionCode) {
+    auto& AssetPaths = Pokemon::FBaseSettings::Get().GetDynamicAssetPaths();
+    const auto &[ClassPath] = AssetPaths.MoveFunctionCodePackageName;
+    auto &ClassPrefix = AssetPaths.MoveFunctionCodePrefix;
+    auto FullPackage = FString::Format(TEXT("{0}/{1}{2}.{1}{2}_C"),
+        { ClassPath, ClassPrefix, FunctionCode.ToString() });
+    if (auto MoveClass = LoadObject<UClass>(nullptr, *FullPackage);
+        MoveClass != nullptr && MoveClass->ImplementsInterface(UBattleMove::StaticClass())) {
+        return MoveClass;
+    }
+
+    return UBaseBattleMove::StaticClass();
+}
