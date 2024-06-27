abf3a52945212fac1f31785e2c4f93cc1fb85007
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-06-26 20:17:54 -0400

    Created some of the basic gameplay effect classes

---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Public/Battle/Moves/BattleMoveFunctionCode.h
@@ -0,0 +1,19 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BattleMoveFunctionCode.generated.h"
+
+/**
+ * The gameplay ability for using a move.
+ */
+UCLASS()
+class POKEMONBATTLE_API UBattleMoveFunctionCode : public UGameplayAbility {
+    GENERATED_BODY()
+
+public:
+    
+
+};
---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Public/Battle/Moves/PayMoveCost.h
@@ -0,0 +1,22 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "PayMoveCost.generated.h"
+
+/**
+ * Gameplay effect used to decrement the PP of a move in battle.
+ */
+UCLASS()
+class POKEMONBATTLE_API UPayMoveCost : public UGameplayEffect {
+    GENERATED_BODY()
+
+public:
+    
+    
+private:
+    
+
+};
