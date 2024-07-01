1767d41429e125fc5cdb560d79023b6f74c50582
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-07-01 15:25:24 -0400

    Updated the test code further

--- Plugins/PokemonStarterKit/Source/PokemonTests/Private/UnitTests/Battle/Battlers/BattlerActorTest.cpp
+++ Plugins/PokemonStarterKit/Source/PokemonTests/Private/UnitTests/Battle/Battlers/BattlerActorTest.cpp
@@ -2,6 +2,8 @@
 #include "Battle/Actions/BattleAction.h"
 #include "Battle/Battle.h"
 #include "Battle/BattleSide.h"
+#include "Battle/Attributes/PokemonCoreAttributeSet.h"
+#include "Battle/Battlers/BattlerAbilityComponent.h"
 #include "Lookup/InjectionUtilities.h"
 #include "Misc/AutomationTest.h"
 #include "Mocking/UnrealMock.h"
@@ -31,13 +33,23 @@ bool BattlerActorTest_Stats::RunTest(const FString &Parameters) {
     auto Pokemon = UnrealInjector::NewInjectedDependency<IPokemon>(
         World.Get(), FPokemonDTO{.Species = TEXT("MIMIKYU"), .Level = 50});
     auto Battler = World->SpawnActor<ATestBattlerActor>();
+    Battler->DispatchBeginPlay(false);
     Battler->Initialize(Side, Pokemon);
 
     UE_CHECK_TRUE(Battler->GetOwningSide() == Side);
     UE_CHECK_EQUAL(Pokemon->GetNickname().ToString(), Battler->GetNickname().ToString());
     UE_CHECK_EQUAL(Pokemon->GetGender(), Battler->GetGender());
     UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetLevel(), Battler->GetPokemonLevel());
+    
+    auto CoreAttributes =  Battler->GetAbilityComponent()->GetCoreAttributes();
+    UE_CHECK_EQUAL(Pokemon->GetCurrentHP(), static_cast<int32>(CoreAttributes->GetHP()));
+    UE_CHECK_EQUAL(Pokemon->GetMaxHP(), static_cast<int32>(CoreAttributes->GetMaxHP()));
     UE_CHECK_EQUAL(1.f, Battler->GetHPPercent());
+    UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetStat("ATTACK")->GetStatValue(), static_cast<int32>(CoreAttributes->GetAttack()));
+    UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetStat("DEFENSE")->GetStatValue(), static_cast<int32>(CoreAttributes->GetDefense()));
+    UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetStat("SPECIAL_ATTACK")->GetStatValue(), static_cast<int32>(CoreAttributes->GetSpecialAttack()));
+    UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetStat("SPECIAL_DEFENSE")->GetStatValue(), static_cast<int32>(CoreAttributes->GetSpecialDefense()));
+    UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetStat("SPEED")->GetStatValue(), static_cast<int32>(CoreAttributes->GetSpeed()));
     UE_CHECK_FALSE(Battler->IsFainted());
     UE_CHECK_EQUAL(Pokemon->GetStatBlock()->GetExpPercent(), Battler->GetExpPercent());
 
@@ -73,6 +85,7 @@ bool TestAiBattlerController::RunTest(const FString &Parameters) {
                     .Level = 50,
                     .Moves = {TEXT("SHADOWSNEAK"), TEXT("PLAYROUGH"), TEXT("SWORDSDANCE"), TEXT("SHADOWCLAW")}});
     auto Battler = World->SpawnActor<ATestBattlerActor>();
+    Battler->DispatchBeginPlay(false);
     Battler->Initialize(Side, Pokemon);
 
     Battler->SelectActions();
