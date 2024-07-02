b5041c95a4826e01c9c0942af6cfaa88683c2244
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-07-02 14:29:08 -0400

    Removed a bunch of unused code

--- Plugins/PokemonStarterKit/Source/PokemonBattle/Private/Battle/Types/TypeMatchUpCalculation.cpp
+++
@@ -1,8 +0,0 @@
-﻿// "Unreal Pokémon" created by Retro & Chill.
-
-
-#include "Battle/Types/TypeMatchUpCalculation.h"
-
-float UTypeMatchUpCalculation::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec &Spec) const {
-    return Super::CalculateBaseMagnitude_Implementation(Spec);
-}
