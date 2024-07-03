4a559c9b139410fb8bb0dd7a87ea606b38b4b814
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-07-02 20:35:19 -0400

    Added tags for the status effects

---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Private/Battle/StatusEffects/StatusEffectTags.cpp
@@ -0,0 +1,26 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Battle/StatusEffects/StatusEffectTags.h"
+#include "DataManager.h"
+#include "Battle/Status.h"
+#include "Battle/Tags.h"
+
+namespace Pokemon::Battle::StatusEffects {
+
+FLookup::FLookup() {
+    auto &DataManager = FDataManager::GetInstance();
+    auto& TypeTable = DataManager.GetDataTable<FStatus>();
+    TypeTable.ForEach([this](const FStatus& Status) {
+        AddDynamicGameplayTag(Tags, StatusEffectTagFormat, Status.ID);
+    });
+}
+
+FLookup::~FLookup() = default;
+
+FLookup& FLookup::GetInstance() {
+    static FLookup Lookup;
+    return Lookup;
+}
+
+}
---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Public/Battle/StatusEffects/StatusEffectTags.h
@@ -0,0 +1,40 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace Pokemon::Battle::StatusEffects {
+
+/**
+ * The format used for the status effect tags
+ */
+constexpr auto StatusEffectTagFormat = TEXT("Battle.StatusEffects.{0}");
+
+/**
+ * Singleton class for looking up status effect tags by their ID
+ */
+class POKEMONBATTLE_API FLookup {
+    FLookup();
+    ~FLookup();
+
+public:
+    /**
+     * Get the singleton instance of this class
+     * @return The sole instance of the class
+     */
+    static FLookup& GetInstance();
+
+    FORCEINLINE const FNativeGameplayTag& GetTag(FName ID) {
+        return *Tags.FindChecked(ID);
+    }
+
+private:
+    /**
+     * The map of status effect IDs to native gameplay tags.
+     */
+    TMap<FName, TSharedRef<FNativeGameplayTag>> Tags;
+};
+
+}
