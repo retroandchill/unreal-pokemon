b18c2088271bca8497da7b0257f168db930f17b3
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-07-13 22:44:41 -0400

    Added the trainer card widget setup

---
+++ Plugins/PokemonStarterKit/Source/PokemonUI/Public/Info/TrainerCardWidgetBase.h
@@ -0,0 +1,108 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "TrainerCardWidgetBase.generated.h"
+
+class UImage;
+class UCommonNumericTextBlock;
+class ITrainer;
+class UPlayerMetadata;
+class UCommonTextBlock;
+/**
+ * Base widget for displaying the contents of the trainer card
+ */
+UCLASS(Abstract)
+class POKEMONUI_API UTrainerCardWidgetBase : public UCommonUserWidget {
+    GENERATED_BODY()
+
+    protected:
+    void NativeConstruct() override;
+
+  private:
+    /**
+     * Set the graphic used for the trainer's sprite
+     */
+    void SetTrainerSprite();
+
+    /**
+     * Set the textual information for the player
+     */
+    void SetTrainerInfo();
+
+    /**
+     * Set the display text for the time
+     * @param Playtime The new playtime
+     */
+    UFUNCTION()
+    void SetPlayerTimeInfo(float Playtime);
+
+    /**
+     * The trainer we're displaying information for
+     */
+    UPROPERTY()
+    TScriptInterface<ITrainer> Trainer;
+
+    /**
+     * The metadata about the player we're looking at
+     */
+    UPROPERTY()
+    TObjectPtr<UPlayerMetadata> PlayerMetadata;
+
+    /**
+     * The image used for the player's sprite
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UImage> TrainerImage;
+
+    /**
+     * The text used to display the player's name
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> TrainerNameText;
+
+    /**
+     * The text used to display the player's ID number
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> IDText;
+
+    /**
+     * The text used to display the player's current playtime
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> TimeText;
+
+    /**
+     * The text used to display the player's current Pokédex progress
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonNumericTextBlock> PokedexText;
+
+    /**
+     * The text used to display the player's current money
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> MoneyText;
+
+    /**
+     * The text used to display the date the player started their adventure on
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> AdventureStartedText;
+
+    /**
+     * The text used to display the date the player first entered the hall of fame
+     */
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UCommonTextBlock> FirstHallOfFameText;
+
+    /**
+     * The length of the player's ID number
+     */
+    UPROPERTY(EditAnywhere, Category = Display, meta = (UIMin = 1, ClampMin = 1))
+    int32 IdNumberLength = 6;
+
+};
--- Plugins/PokemonStarterKit/Source/PokemonUI/Public/Menus/PauseMenuBase.h
+++ Plugins/PokemonStarterKit/Source/PokemonUI/Public/Menus/PauseMenuBase.h
@@ -15,30 +15,4 @@ UCLASS(Abstract)
 class POKEMONUI_API UPauseMenuBase : public UPokemonMenuBase {
     GENERATED_BODY()
 
-public:
-    void NativeConstruct() override;
-
-protected:
-    /**
-     * Slot the button for a given Pokémon into the widget
-     * @param Button The button to place into the slot
-     */
-    UFUNCTION(BlueprintImplementableEvent, Category = "Pokémon")
-    void SlotPokemonButton(UPokemonInfoButtonBase* Button);
-
-private:
-    UPokemonInfoButtonBase* CreatePokemonPanel(const TScriptInterface<IPokemon>& Pokemon);
-    
-    /**
-     * The class used for a Pokémon panel
-     */
-    UPROPERTY(EditAnywhere, Category = "Pokémon")
-    TSubclassOf<UPokemonInfoButtonBase> PokemonPanelClass;
-
-    /**
-     * The category used to display the information to the player
-     */
-    UPROPERTY()
-    TArray<TObjectPtr<UPokemonInfoButtonBase>> PokemonPanels;
-
 };
