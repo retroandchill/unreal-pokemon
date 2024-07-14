9233e56499a04f473d15fac0a3e38aa3e83e6545
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-07-14 16:41:04 -0400

    Fixed issues with the conversion

---
+++ Plugins/RPGMenus/Source/RPGMenus/Private/RPGUIManagerSubsystem.cpp
@@ -0,0 +1,12 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "RPGUIManagerSubsystem.h"
+#include "PrimaryGameLayout.h"
+#include "Screens/Screen.h"
+
+UScreen * URPGUIManagerSubsystem::GetTopScreenOfStack() const {
+    auto Layout = UPrimaryGameLayout::GetPrimaryGameLayoutForPrimaryPlayer(this);
+    auto Layer = Layout->GetLayerWidget(RPG::Menus::PrimaryMenuLayerTag);
+    return Cast<UScreen>(Layer->GetActiveWidget());
+}
