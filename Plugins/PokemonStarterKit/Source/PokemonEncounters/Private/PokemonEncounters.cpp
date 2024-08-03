9f6ec2a603eb368b44d0e8b373ed9c7c66a0ec32
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-03 13:17:48 -0400

    Created the encounters module

---
+++ Plugins/PokemonStarterKit/Source/PokemonEncounters/Private/PokemonEncounters.cpp
@@ -0,0 +1,11 @@
+﻿#include "PokemonEncounters.h"
+
+void FPokemonEncountersModule::StartupModule() {
+    // No implementation needed
+}
+
+void FPokemonEncountersModule::ShutdownModule() {
+    // No implementation needed
+}
+    
+IMPLEMENT_MODULE(FPokemonEncountersModule, PokemonEncounters)
