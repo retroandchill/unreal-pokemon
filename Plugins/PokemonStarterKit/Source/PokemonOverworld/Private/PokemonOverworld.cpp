d87f30338f5ad42603dc7c4fc0db5eed9e0f4c58
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-03 20:10:07 -0400

    Set up the ability component to hold the encounter attributes

---
+++ Plugins/PokemonStarterKit/Source/PokemonOverworld/Private/PokemonOverworld.cpp
@@ -0,0 +1,13 @@
+﻿#include "PokemonOverworld.h"
+
+void FPokemonOverworldModule::StartupModule()
+{
+    // No implementation
+}
+
+void FPokemonOverworldModule::ShutdownModule()
+{
+    // No implementation
+}
+    
+IMPLEMENT_MODULE(FPokemonOverworldModule, PokemonOverworld)
