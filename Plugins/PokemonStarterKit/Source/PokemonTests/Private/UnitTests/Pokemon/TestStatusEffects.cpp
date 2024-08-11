1e3c8c54fa59ef86e2ae5659d0cd6c4277c9be82
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-11 19:37:17 -0400

    Added a test to cover status conditions

---
+++ Plugins/PokemonStarterKit/Source/PokemonTests/Private/UnitTests/Pokemon/TestStatusEffects.cpp
@@ -0,0 +1,39 @@
+﻿#include "Asserts.h"
+#include "Managers/PokemonSubsystem.h"
+#include "Misc/AutomationTest.h"
+#include "Pokemon/GamePokemon.h"
+#include "Utilities/StatusEffectUtilities.h"
+#include "Utilities/WidgetTestUtilities.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(TestStatusEffects, "Unit Tests.Pokemon.TestStatusEffects",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool TestStatusEffects::RunTest(const FString &Parameters) {
+    auto [DudOverlay, World, GameInstance] = UWidgetTestUtilities::CreateTestWorld();
+
+    UPokemonSubsystem::GetInstance(World.Get()).StartNewGame();
+    auto Player = UPokemonSubsystem::GetInstance(World.Get()).GetPlayer();
+    auto Pokemon = NewObject<UGamePokemon>(World.Get());
+    Pokemon->Initialize({.Species = TEXT("PIKACHU"), .Level = 20}, Player);
+
+    FStatus Status;
+    UE_CHECK_EQUAL(EStatusEffectStatus::NoStatusEffect, UStatusEffectUtilities::GetStatusEffect(Pokemon, Status));
+
+    Pokemon->SetStatusEffect("BURN");
+    UE_CHECK_EQUAL(EStatusEffectStatus::HasStatusEffect, UStatusEffectUtilities::GetStatusEffect(Pokemon, Status));
+    UE_CHECK_EQUAL(TEXT("BURN"), Status.ID.ToString());
+    
+    Pokemon->SetStatusEffect("SLEEP");
+    UE_CHECK_EQUAL(EStatusEffectStatus::HasStatusEffect, UStatusEffectUtilities::GetStatusEffect(Pokemon, Status));
+    UE_CHECK_EQUAL(TEXT("BURN"), Status.ID.ToString());
+
+    
+    Pokemon->SetStatusEffect("SLEEP", true);
+    UE_CHECK_EQUAL(EStatusEffectStatus::HasStatusEffect, UStatusEffectUtilities::GetStatusEffect(Pokemon, Status));
+    UE_CHECK_EQUAL(TEXT("SLEEP"), Status.ID.ToString());
+
+    Pokemon->RemoveStatusEffect();
+    UE_CHECK_EQUAL(EStatusEffectStatus::NoStatusEffect, UStatusEffectUtilities::GetStatusEffect(Pokemon, Status));
+    
+    return true;
+}
