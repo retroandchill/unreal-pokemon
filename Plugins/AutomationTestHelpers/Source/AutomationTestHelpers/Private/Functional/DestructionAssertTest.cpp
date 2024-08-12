01438ae863e91916aea9d89a0f0233e137afc6b2
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-11 21:26:59 -0400

    Created a new test to check the reset behavior

---
+++ Plugins/AutomationTestHelpers/Source/AutomationTestHelpers/Private/Functional/DestructionAssertTest.cpp
@@ -0,0 +1,10 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Functional/DestructionAssertTest.h"
+
+
+void ADestructionAssertTest::EndPlay(const EEndPlayReason::Type EndPlayReason) {
+    FinalAssert(EndPlayReason);
+    Super::EndPlay(EndPlayReason);
+}
