3660b8cd7500950c1fc5548009c022040e863b11
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-21 20:18:28 -0400

    Create the 3D Tilemap plugin

---
+++ Plugins/Tilemap3D/Source/Tilemap3D/Public/Tilemap/Tilemap3D.h
@@ -0,0 +1,16 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tilemap3D.generated.h"
+
+UCLASS()
+class TILEMAP3D_API ATilemap3D : public AActor {
+    GENERATED_BODY()
+
+public:
+
+
+};
