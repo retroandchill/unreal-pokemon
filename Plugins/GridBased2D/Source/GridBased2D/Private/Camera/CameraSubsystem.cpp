f3fe28d2ed8d2e4761358de690061dec341b5a53
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-08-21 20:03:50 -0400

    Fixed test failure issues

--- Plugins/GridBased2D/Source/GridBased2D/Private/Camera/CameraSubsystem.cpp
+++ Plugins/GridBased2D/Source/GridBased2D/Private/Camera/CameraSubsystem.cpp
@@ -2,27 +2,36 @@
 
 #include "Camera/CameraSubsystem.h"
 #include "Kismet/GameplayStatics.h"
+#include "Ranges/Optional/IfPresent.h"
+#include "Ranges/Optional/Map.h"
+#include "Ranges/Optional/OptionalClosure.h"
+#include "Ranges/Optional/OptionalRef.h"
 
 void UCameraSubsystem::OnWorldBeginPlay(UWorld &InWorld) {
     Super::OnWorldBeginPlay(InWorld);
 
-    auto PlayerIndex = InWorld.GetGameInstance()->GetPrimaryPlayerController(false)
-        ->GetLocalPlayer()->GetLocalPlayerIndex();
-    PlayerCameraManager = UGameplayStatics::GetPlayerCameraManager(&InWorld, PlayerIndex);
+    UE::Optionals::OfNullable(InWorld.GetGameInstance()) |
+        UE::Optionals::Map(&UGameInstance::GetPrimaryPlayerController, false) |
+        UE::Optionals::Map(&APlayerController::GetLocalPlayer) |
+        UE::Optionals::Map(&ULocalPlayer::GetLocalPlayerIndex) |
+        UE::Optionals::IfPresent([this, &InWorld](int32 PlayerIndex) {
+            PlayerCameraManager = UGameplayStatics::GetPlayerCameraManager(&InWorld, PlayerIndex);
+        });
+
 }
 
 void UCameraSubsystem::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
     auto CameraRotation = PlayerCameraManager->GetCameraRotation();
     if (FVector2D NewRotation(CameraRotation.Yaw + ZRotationAdjustment, CameraRotation.Pitch);
-            !CurrentCameraAngle.Equals(NewRotation)) {
+        !CurrentCameraAngle.Equals(NewRotation)) {
         CurrentCameraAngle = NewRotation;
         OnCameraAngleChanged.Broadcast(CurrentCameraAngle);
     }
 }
 
 TStatId UCameraSubsystem::GetStatId() const {
-	RETURN_QUICK_DECLARE_CYCLE_STAT(UCameraSubsystem, STATGROUP_Tickables);
+    RETURN_QUICK_DECLARE_CYCLE_STAT(UCameraSubsystem, STATGROUP_Tickables);
 }
 
 FOnCameraAngleChanged &UCameraSubsystem::GetOnCameraAngleChanged() {
