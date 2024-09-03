80c19a5cd24f596cdc2a467bb68df4423333d11a
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-09-03 18:01:53 -0400

    Added a small test for the switch on data handle

---
+++ Plugins/PokemonStarterKit/Source/PokemonTests/Private/UnitTests/Nodes/TestSwitchOnDataHandle.cpp
@@ -0,0 +1,29 @@
+﻿#include "Asserts.h"
+#include "BlueprintActionDatabase.h"
+#include "Data/K2Node_IsGameDataIDValid.h"
+#include "Data/K2Node_SwitchOnDataHandle.h"
+#include "Kismet2/BlueprintEditorUtils.h"
+#include "Kismet2/KismetEditorUtilities.h"
+#include "Misc/AutomationTest.h"
+#include "Utilities/K2Nodes.h"
+#include "Species/Ability.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(TestSwitchOnDataHandle, "Unit Tests.Nodes.TestSwitchOnDataHandle",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool TestSwitchOnDataHandle::RunTest(const FString &Parameters) {
+    using enum ENodeTitleType::Type;
+    FBlueprintActionDatabase::Get().RefreshAll();
+
+    MakeTestableBP(TestBP, TestGraph);
+
+    auto TestNode = NewObject<UK2Node_SwitchOnDataHandle>(TestGraph.Get());
+    UE_CHECK_EQUAL(TEXT("Switch on <<Invalid>>"), TestNode->GetNodeTitle(MenuTitle).ToString());
+
+    TestNode->Initialize(FAbilityHandle::StaticStruct());
+    UE_CHECK_EQUAL(TEXT("Switch on Ability Handle"), TestNode->GetNodeTitle(MenuTitle).ToString());
+    UE_CHECK_EQUAL(TEXT("Selects an output that matches the input value"), TestNode->GetTooltipText().ToString());
+    UE_CHECK_TRUE(TestNode->ShouldShowNodeProperties());
+    
+    return true;
+}
