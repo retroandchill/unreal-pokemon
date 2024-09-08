7bc574460a799cb80c0c9e1ed33259453085e7bc
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-09-08 18:14:49 -0400

    Set up basic materials for a simple flipbook

---
+++ Plugins/Enhanced2D/Source/Enhanced2D/Private/Enhanced2D.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Enhanced2D.h"
+
+#define LOCTEXT_NAMESPACE "FEnhanced2DModule"
+
+void FEnhanced2DModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FEnhanced2DModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEnhanced2DModule, Enhanced2D)
