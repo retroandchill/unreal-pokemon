17f52d1bca68108da5731fc5656c1c23d067e326
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-09-25 19:28:42 -0400

    Revert "Revert "Set up a new way to expand template arguments""
    This reverts commit f33e6a7490c4030d59b265b70521c6c6d4f9ce9b.

---
+++ Plugins/UnrealRanges/Source/UnrealRanges/Public/Ranges/Functional/TryInvoke.h
@@ -0,0 +1,26 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ranges/Concepts/Tuples.h"
+
+namespace UE::Ranges {
+
+    class FTryInvoke {
+    public:
+        template <typename F, typename... A>
+        auto operator()(F&& Functor, A&&... Args) const {
+            return std::invoke(Forward<F>(Functor), Forward<A>(Args)...);
+        }
+
+        template <typename F, typename T>
+            requires CanApply<F, T>
+        auto operator()(F&& Functor, T&& Tuple) const {
+            return std::apply(Forward<F>(Functor), Forward<T>(Tuple));
+        }
+    };
+
+    constexpr auto TryInvoke = FTryInvoke();
+    
+}
