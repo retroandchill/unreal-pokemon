cb38989442991dec6ee324005f4c0a30c7849cf7
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-09-25 19:28:35 -0400

    Added a new tuple mapper

---
+++ Plugins/UnrealRanges/Source/UnrealRanges/Private/Ranges/Views/MapTuple.cpp
@@ -0,0 +1,11 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Ranges/Views/MapTuple.h"
+
+
+MapTuple::MapTuple() {
+}
+
+MapTuple::~MapTuple() {
+}
---
+++ Plugins/UnrealRanges/Source/UnrealRanges/Public/Ranges/Views/MapTuple.h
@@ -0,0 +1,25 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ranges/Views/Map.h"
+
+namespace UE::Ranges {
+
+    struct FMapTuple {
+        template <typename... A>
+        constexpr auto operator()(A &&...Args) const {
+            return Map([]<typename T>(T&& Tuple) {
+                
+            }, Forward<A>(Args)...);
+        }
+    };
+
+    /**
+     * Equivalent to ranges::views::transform in the STL/range-v3, this version differs in that there are several things
+     * you can supply to it in order to create a valid binding.
+     */
+    constexpr FMapTuple MapTuple;
+
+} // namespace UE::Ranges
