248d1eaad4819f9c1970c480f8e7f7cceff68a02
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-09-28 14:55:37 -0400

    Simplified the deposit algorithm

--- Plugins/PokemonStarterKit/Source/PokemonCore/Private/Storage/DefaultStorageSystem.cpp
+++ Plugins/PokemonStarterKit/Source/PokemonCore/Private/Storage/DefaultStorageSystem.cpp
@@ -6,7 +6,6 @@
 #include "Ranges/Algorithm/ToArray.h"
 #include "Ranges/Optional/Construct.h"
 #include "Ranges/Optional/FlatMapTuple.h"
-#include "Ranges/Optional/MapTuple.h"
 #include "Ranges/Views/Enumerate.h"
 #include "Ranges/Views/FilterTuple.h"
 #include "Ranges/Views/Ints.h"
@@ -15,16 +14,10 @@
 #include "Settings/PokemonStorageSystemSettings.h"
 #include "Storage/StorageBox.h"
 
-static FText GetDefaultBoxName(int32 Index) {
-    auto Settings = GetDefault<UPokemonStorageSystemSettings>();
-    return FText::FormatNamed(Settings->BoxNameFormat, TEXT("Index"), Index + 1);
-}
-
+static FText GetDefaultBoxName(int32 Index);
+static bool CheckOpenBox(int32 Index, const TScriptInterface<IStorageBox> &Box);
 static TOptional<FDepositResult> TryDepositToBox(int32 Index, const TScriptInterface<IStorageBox> &Box,
-    const TScriptInterface<IPokemon> &Pokemon) {
-    return Box->DepositToBox(Pokemon) |
-        UE::Optionals::Construct<FDepositResult>(Index);
-}
+    const TScriptInterface<IPokemon> &Pokemon);
 
 TScriptInterface<IStorageSystem> UDefaultStorageSystem::Initialize(int32 BoxCount, int32 BoxCapacity,
                                                                    int32 StartingIndex) {
@@ -81,14 +74,11 @@ void UDefaultStorageSystem::SetCurrentBoxIndex(int32 NewIndex) {
 TOptional<FDepositResult> UDefaultStorageSystem::TryDeposit(const TScriptInterface<IPokemon> &Pokemon) {
     check(Boxes.IsValidIndex(CurrentBoxIndex))
     std::array Indexes = {UE::Ranges::Ints(CurrentBoxIndex, Boxes.Num()), UE::Ranges::Ints(0, CurrentBoxIndex)};
-    auto Binding = UE::Optionals::MapTuple(&TryDepositToBox, Pokemon);
     // clang-format off
     return Indexes |
                    UE::Ranges::Join |
                    UE::Ranges::ReverseEnumerate<int32>(Boxes) |
-                   UE::Ranges::FilterTuple([](int32, const TScriptInterface<IStorageBox> &Box) {
-                       return Box->IsBoxFull();
-                   }) |
+                   UE::Ranges::FilterTuple(&CheckOpenBox) |
                    UE::Ranges::FindFirst |
                    UE::Optionals::FlatMapTuple(&TryDepositToBox, Pokemon);
     // clang-format on
@@ -96,4 +86,19 @@ TOptional<FDepositResult> UDefaultStorageSystem::TryDeposit(const TScriptInterfa
 
 TScriptInterface<IStorageBox> UDefaultStorageSystem::CreateStorageBox(FText &&BoxName, int32 BoxCapacity) {
     return UnrealInjector::NewInjectedDependency<IStorageBox>(this, MoveTemp(BoxName), BoxCapacity);
+}
+
+static FText GetDefaultBoxName(int32 Index) {
+    auto Settings = GetDefault<UPokemonStorageSystemSettings>();
+    return FText::FormatNamed(Settings->BoxNameFormat, TEXT("Index"), Index + 1);
+}
+
+static bool CheckOpenBox(int32, const TScriptInterface<IStorageBox> &Box) {
+    return !Box->IsBoxFull();
+}
+
+static TOptional<FDepositResult> TryDepositToBox(int32 Index, const TScriptInterface<IStorageBox> &Box,
+    const TScriptInterface<IPokemon> &Pokemon) {
+    return Box->DepositToBox(Pokemon) |
+        UE::Optionals::Construct<FDepositResult>(Index);
 }
