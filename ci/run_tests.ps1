da41371bf6c9027c900512e7ab85c630cfeed909
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-13 19:31:13 -0400

    Updated the LLVM location

--- ci/run_tests.ps1
+++ ci/run_tests.ps1
@@ -1,11 +1,16 @@
 $location = $( Get-Location )
 $unreal_engine_location = "C:/Program Files/Epic Games/UE_5.4/Engine/Binaries/Win64"
+echo "Running tests..."
 & "${unreal_engine_location}/UnrealEditor-Win64-DebugGame-Cmd.exe" "${location}/UnrealPokemon.uproject" -clientconfig=DebugGame -nosplash -unattended -nullrhi -execcmds="automation runtests Unit Tests+Functional Tests+Python.UnrealPokemon,ShutdownAfterTests" -log -ReportOutputPath="${$location}/TestResults"
 if ($LASTEXITCODE -ne 0) {
+    echo "Tests failed with exit code: ${$LASTEXITCODE}"
     exit -1
 }
+
+echo "Tests Passed!"
 Copy-Item "${unreal_engine_location}/default.profraw" "${location}/default.profraw"
 
-$clang_cl_location = "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/"
+echo "Outputing coverage data"
+$clang_cl_location = "C:/Program Files/Llvm/bin/"
 & "${clang_cl_location}/llvm-profdata.exe" merge --output=unreal.profdata *.profraw
 python ./ci/produce_coverage_data.py "${clang_cl_location}" unreal.profdata
