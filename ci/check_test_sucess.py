be1df375b9e8fc406404215cc2f41e3dc9a5419f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-13 21:09:24 -0400

    Changed how the test failures are tracked

---
+++ ci/check_test_sucess.py
@@ -0,0 +1,13 @@
+﻿import json
+import sys
+
+if __name__ == "__main__":
+    output_log = sys.argv[1]
+    
+    with open(output_log, 'r', encoding='utf-8-sig') as f:
+        data = json.load(f)
+
+    if 'failed' in data:
+        print(data['failed'])
+    else:
+        print(-1)
--- ci/run_tests.ps1
+++ ci/run_tests.ps1
@@ -3,8 +3,9 @@ echo "Location: $location"
 $unreal_engine_location = "C:/Program Files/Epic Games/UE_5.4/Engine/Binaries/Win64"
 echo "Running tests..."
 & "${unreal_engine_location}/UnrealEditor-Win64-DebugGame-Cmd.exe" "${location}/UnrealPokemon.uproject" -clientconfig=DebugGame -nosplash -unattended -nullrhi -execcmds="automation runtests Unit Tests+Functional Tests+Python.UnrealPokemon,ShutdownAfterTests" -log -ReportOutputPath="${location}/TestResults"
-if ($LASTEXITCODE -ne 0) {
-    echo "Tests failed with exit code: ${LASTEXITCODE}"
+$result = $(python ./ci/check_test_success.py TestResults/index.json)
+if ($result -ne 0) {
+    echo "$result tests failed!"
     exit -1
 }
 
