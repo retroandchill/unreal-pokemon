e2f64eb9585a44dbf10c9dbf27c38ce07c0276fc
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-13 22:17:31 -0400

    Just used MSVC for the testing, even if we take a coverage hit

--- ci/run_tests.ps1
+++ ci/run_tests.ps1
@@ -2,16 +2,8 @@ $location = $( Get-Location )
 echo "Location: $location"
 $unreal_engine_location = "C:/Program Files/Epic Games/UE_5.4/Engine/Binaries/Win64"
 echo "Running tests..."
-& "${unreal_engine_location}/UnrealEditor-Win64-DebugGame-Cmd.exe" "${location}/UnrealPokemon.uproject" -clientconfig=DebugGame -nosplash -unattended -nullrhi -execcmds="automation runtests Unit Tests+Functional Tests+Python.UnrealPokemon,ShutdownAfterTests" -log -ReportOutputPath="${location}/TestResults"
+OpenCppCoverage --modules ${location}\**\Binaries\Win64 --sources ${location}\Source\**\* --sources ${location}\Plugins\**\* --excluded_sources .gen. --excluded_sources **\External\** --excluded_sources **\Deps\** --excluded_sources .gen. --export_type cobertura:cpp-coverage.xml --export_type html:codecoverage/Report --optimized_build --excluded_line_regex ".*\(.*\)\s*(?:noexcept)?\s*=\s*default;.*" --excluded_line_regex ".*GENERATED_BODY\(\).*" -- "${unreal_engine_location}/UnrealEditor-Win64-DebugGame-Cmd.exe" "${location}/UnrealPokemon.uproject" -clientconfig=DebugGame -nosplash -unattended -nullrhi -execcmds="automation runtests Unit Tests+Functional Tests+Python.UnrealPokemon,ShutdownAfterTests" -log -ReportOutputPath="${location}/TestResults"
 if ($LASTEXITCODE -ne 0) {
     echo "Tests failed with exit code: ${LASTEXITCODE}"
     exit -1
 }
-
-echo "Tests Passed!"
-Copy-Item "${unreal_engine_location}/default.profraw" "${location}/default.profraw"
-
-echo "Outputing coverage data"
-$clang_cl_location = "C:/Program Files/Llvm/bin/"
-& "${clang_cl_location}/llvm-profdata.exe" merge --output=unreal.profdata *.profraw
-python ./ci/produce_coverage_data.py "${clang_cl_location}" unreal.profdata
