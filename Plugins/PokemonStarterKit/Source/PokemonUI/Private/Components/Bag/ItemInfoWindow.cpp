43e531929d384df0ecf3fc3cc187f55b2a44fde9
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-22 21:42:09 -0400

    Continued to refactor the design of the UI

--- Plugins/PokemonStarterKit/Source/PokemonUI/Private/Components/Bag/ItemInfoWindow.cpp
+++ Plugins/PokemonStarterKit/Source/PokemonUI/Private/Components/Bag/ItemInfoWindow.cpp
@@ -7,19 +7,3 @@
 #include "Graphics/AssetClasses.h"
 #include "Ranges/Optional/GetPtrOrNull.h"
 #include "Utilities/WidgetUtilities.h"
-
-void UItemInfoWindow::Refresh_Implementation(const FItem &Item, int32 Quantity) {
-    // clang-format off
-    auto ItemGraphic = Pokemon::Assets::Graphics::ItemIcons.LoadAsset(Item.ID) |
-                       UE::Optionals::Map([](const FImageAsset &Asset) -> auto &{ return Asset.Get(); }) |
-                       UE::Optionals::GetPtrOrNull;
-    // clang-format on
-    ItemIcon->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
-    UWidgetUtilities::SetBrushFromAsset(ItemIcon, ItemGraphic, true);
-    ItemDescription->SetText(Item.Description);
-}
-
-void UItemInfoWindow::ClearItem_Implementation() {
-    ItemIcon->SetVisibility(ESlateVisibility::Hidden);
-    ItemDescription->SetText(FText::GetEmpty());
-}
