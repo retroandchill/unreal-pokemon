a160ee53f47618ec1dccfe65f0fef91dd9591009
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-25 18:46:19 -0400

    Moved unreachable into its own file

--- Plugins/UnrealRanges/Source/UnrealRanges/Public/Ranges/Utilities/Methods.h
+++ Plugins/UnrealRanges/Source/UnrealRanges/Public/Ranges/Utilities/Methods.h
@@ -3,16 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
-namespace UE::Ranges {
-    [[noreturn]] inline void Unreachable() {
-#if defined(_MSC_VER) && !defined(__clang__) // MSVC
-        __assume(false);
-#else // GCC, Clang
-        __builtin_unreachable();
-#endif
-    }
-}
+#include "Unreachable.h"
 
 #if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
 #define __PRETTY_FUNCTION__ __FUNCSIG__
