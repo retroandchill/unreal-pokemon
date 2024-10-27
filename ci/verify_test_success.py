5066893f34334bf7a1d7c7158332bef5ff6207c9
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-10-27 12:49:26 -0400

    Fix to the menu categories of the asset loading nodes (#183)
    * Fix to the menu categories of the asset loading nodes
    
    * SLight test failure fix
    
    * Added a catch for if a test fails in CI
    
    * Fixed the CI script
    
    * Fixed python formatting
    
    * Actually fixed the running script
    
    * removed bad tests

---
+++ ci/verify_test_success.py
@@ -0,0 +1,20 @@
+import json
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-f', '--file', type=str)
+
+    args = parser.parse_args()
+    try:
+        with open(args.file, mode='r', encoding='utf-8-sig') as file:
+            test_report = json.load(file)
+    except IOError:
+        print(f'Failed to open file: {args.file}')
+        exit(1)
+
+    if test_report['failed'] > 0:
+        print(f'{test_report["failed"]} tests failed!')
+        exit(1)
+    else:
+        print('All tests passed!')
