61eb1fb47bd566e88092a969ba5120fafb98fb12
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-02 18:51:27 -0400

    Created some new serialization classes

---
+++ Plugins/EnhancedSaveGame/Source/EnhancedSaveGame/Private/Saving/Serialization/SaveSerializationUtils.cpp
@@ -0,0 +1,40 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Saving/Serialization/SaveSerializationUtils.h"
+#include "Serialization/ObjectAndNameAsStringProxyArchive.h"
+
+FObjectData FObjectData::SerializeObject(UObject *Object) {
+    if (!IsValid(Object)) {
+        return FObjectData();
+    }
+
+    FObjectData Data = { .ObjectClass = Object->GetClass() };
+    FMemoryWriter Writer(Data.Data, true);
+    FObjectAndNameAsStringProxyArchive Archive(Writer, true);
+    Writer.SetIsSaving(true);
+    Object->Serialize(Archive);
+    return Data;
+}
+
+UObject* FObjectData::DeserializeObject() const {
+    if (!IsValid(ObjectClass) || Data.Num() <= 0) {
+        return nullptr;
+    }
+     
+    FMemoryReader Reader(Data, true);
+    FObjectAndNameAsStringProxyArchive Archive(Reader, true);
+    Reader.SetIsLoading(true);
+
+    auto Object = NewObject<UObject>(GetTransientPackage(), ObjectClass);
+    Object->Serialize(Archive);
+    return Object;
+}
+
+FObjectData USaveSerializationUtils::SerializeObject(UObject *Object) {
+    return FObjectData::SerializeObject(Object);
+}
+
+UObject * USaveSerializationUtils::DeserializeObject(const FObjectData &SerializedData) {
+    return SerializedData.DeserializeObject();
+}
---
+++ Plugins/EnhancedSaveGame/Source/EnhancedSaveGame/Private/Saving/Serialization/Serializable.cpp
@@ -0,0 +1,7 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Saving/Serialization/Serializable.h"
+
+
+// Add default functionality here for any ISerializable functions that are not pure virtual.
