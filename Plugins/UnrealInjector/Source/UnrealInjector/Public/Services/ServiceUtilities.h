888876514e6d18e321cbc5f5c66505670301a4c3
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-03 18:35:30 -0500

    Created a new way to get all the classes in the game

---
+++ Plugins/UnrealInjector/Source/UnrealInjector/Public/Services/ServiceUtilities.h
@@ -0,0 +1,22 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ranges/Views/AnyView.h"
+
+class UService;
+
+namespace UnrealInjector {
+
+    /**
+     * Retrieves a unique view of all available service classes.
+     *
+     * This method fetches and returns a list of all the services
+     * that are currently available in the system.
+     *
+     * @return A list of classes, where each string represents the name of a service.
+     */
+    UNREALINJECTOR_API UE::Ranges::TAnyView<TSubclassOf<UService>> GetAllServices(); 
+    
+}
