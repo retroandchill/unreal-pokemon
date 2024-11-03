25f9120b9e28617f39c3c7b0420c92966329548f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-03 13:07:10 -0500

    Removed some unused data classes

--- Plugins/UnrealRanges/Source/UnrealRanges/Private/Ranges/Variants/OpaqueEnum.cpp
+++
@@ -1,6 +0,0 @@
-﻿// "Unreal Pokémon" created by Retro & Chill.
-
-#include "Ranges/Variants/OpaqueEnum.h"
-
-UE::Ranges::FOpaqueEnum::FOpaqueEnum(UEnum *Enum, int64 Value) : NumericValue(Value), Enum(Enum) {
-}
--- Plugins/UnrealRanges/Source/UnrealRanges/Private/Ranges/Variants/Wildcard.cpp
+++
@@ -1,4 +0,0 @@
-﻿// "Unreal Pokémon" created by Retro & Chill.
-
-
-#include "Ranges/Variants/Wildcard.h"
