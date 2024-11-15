7886ba4105712515f8f7bb01d3cefc7e8accc750
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-14 19:43:44 -0500

    Updated the way multipliers work to reflect new GAS changes

--- Plugins/DynamicAssetLoader/Source/DynamicAssetLoaderNodes/Private/Assets/K2Node_DynamicAssetLoadBase.cpp
+++ Plugins/DynamicAssetLoader/Source/DynamicAssetLoaderNodes/Private/Assets/K2Node_DynamicAssetLoadBase.cpp
@@ -255,10 +255,9 @@ void UK2Node_DynamicAssetLoadBase::SetWildcardMode(bool bNewWildcardMode) {
 
     auto AssetNamePin = GetAssetNamePin();
     // clang-format off
-    auto ToBreak = AssetNamePin->LinkedTo |
+    AssetNamePin->LinkedTo |
                    UE::Ranges::Filter([](const UEdGraphPin *Pin) { return Pin->PinType.PinCategory != UEdGraphSchema_K2::PC_String; }) |
-                   UE::Ranges::ToArray;
-    ToBreak | UE::Ranges::ForEach(AssetNamePin, &UEdGraphPin::BreakLinkTo, true);
+                   UE::Ranges::ForEach(AssetNamePin, &UEdGraphPin::BreakLinkTo, true);
     // clang-format on
     RefreshAssetNamePin();
 }
