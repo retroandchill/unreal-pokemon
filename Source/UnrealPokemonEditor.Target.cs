5e3a40664f12bad0432028936ffc6a7a824b92d0
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-16 13:34:34 -0500

    Added support for the code coverage flag

--- Source/UnrealPokemonEditor.Target.cs
+++ Source/UnrealPokemonEditor.Target.cs
@@ -11,6 +11,9 @@ public class UnrealPokemonEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		// This does nothing now, but once https://github.com/EpicGames/UnrealEngine/pull/12561 is merged, then this 
+		// will automatically enable code coverage on Clang-CL builds.
+		bCodeCoverage = true; 
 		RegisterModulesCreatedByRider();
 	}
 
