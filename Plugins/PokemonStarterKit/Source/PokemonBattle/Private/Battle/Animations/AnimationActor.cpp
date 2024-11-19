7ca95c7911244aa09458f4674a31d24c7890c149
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-19 08:50:55 -0500

    Added an actor-based animation class

---
+++ Plugins/PokemonStarterKit/Source/PokemonBattle/Private/Battle/Animations/AnimationActor.cpp
@@ -0,0 +1,18 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "Battle/Animations/AnimationActor.h"
+
+void AAnimationActor::BindDelegateToAnimationComplete_Implementation(const FBattleAnimationCompleteCallback &Delegate) {
+    OnBattleAnimationComplete.Add(Delegate);
+}
+
+void AAnimationActor::RemoveDelegateFromAnimationComplete_Implementation(
+    const FBattleAnimationCompleteCallback &Delegate) {
+    OnBattleAnimationComplete.Remove(Delegate);
+}
+
+void AAnimationActor::AnimationComplete() {
+    OnBattleAnimationComplete.Broadcast();
+    Destroy();
+}
