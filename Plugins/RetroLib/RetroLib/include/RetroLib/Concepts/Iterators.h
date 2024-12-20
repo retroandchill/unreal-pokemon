5585adffee82645aca9946ec91ce9a265673ad23
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-12-20 13:31:11 -0500

    Added ConcatView in order to join various range types together (#18)
    * Added the concepts for creating a concatenation view
    
    * Copied over the basic range structure from range-v3 for ConcatView
    
    * Added the functional executor for concat
    
    * Added a test to verify that the concat view works as expected
    
    * Added a test for iterating up
    
    * Try including memory in here
    
    * Removed the mock Stl import to use the real one instead
    
    * Fix a path issue
    
    * Try using this CMake config
    
    * Try building libcxx to get the module
    
    * Try getting clang-support this way
    
    * Try using cmake 30
    
    * Added the cmake flag
    
    * Add some more installs to get libc++ installed
    
    * Try forcing it to use version 18
    
    * Pin it to do it this way
    
    * Maybe this one works
    
    * Go back to using libc++
    
    * Display everything in the user library
    
    * Try getting the modules enabled this way
    
    * Do a log trace on the build
    
    * Try setting the values this way
    
    * Log is the variables are being set
    
    * Set the build dir this way
    
    * Verbose log
    
    * Put the log level down to debug
    
    * Try creating the module this way
    
    * Remove the debug logging
    
    * Try manually doing the include
    
    * Try using this to break the recursion.
    
    * Reverse that change
    
    * Maybe this will break the deduction issue
    
    * Add an explicit return type function
    
    * Fixed includes
    
    * Added more tests and fixed some bugs
    
    * Added some distance logic

---
+++ Plugins/RetroLib/RetroLib/include/RetroLib/Concepts/Iterators.h
@@ -0,0 +1,22 @@
+/**
+ * @file Iterators.h
+ * @brief Concepts for handling iterators.
+ *
+ * @author Retro & Chill
+ * https://github.com/retroandchill
+ */
+
+#pragma once
+
+#if !RETROLIB_WITH_MODULES
+#include <ranges>
+#endif
+
+#ifndef RETROLIB_EXPORT
+#define RETROLIB_EXPORT
+#endif
+
+namespace retro {
+    template <typename I>
+    concept SinglePassIterator = std::input_or_output_iterator<I> && !std::forward_iterator<I>;
+}
