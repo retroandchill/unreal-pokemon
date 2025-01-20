4406a970944f16d9ec0154479a0ac6bdecc73e19
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-01-20 18:48:33 -0500

    Created the simple flipbook type's basic structure

--- Plugins/Simple2D/Source/Simple2D/Private/SimpleFlipbook/Assets/SimpleFlipbook.cpp
+++ Plugins/Simple2D/Source/Simple2D/Private/SimpleFlipbook/Assets/SimpleFlipbook.cpp
@@ -2,8 +2,52 @@
 
 
 #include "SimpleFlipbook/Assets/SimpleFlipbook.h"
+#include "PaperSprite.h"
 
 USimpleFlipbook::USimpleFlipbook() {
     static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaskedMaterialRef(TEXT("/Paper2D/MaskedUnlitSpriteMaterial"));
     DefaultMaterial = MaskedMaterialRef.Object;
 }
+
+float USimpleFlipbook::GetTotalDuration() const {
+    if (FramesPerSecond != 0) {
+        return static_cast<float>(GetNumFrames()) / FramesPerSecond;
+    }
+
+    return 0.0f;
+}
+
+int32 USimpleFlipbook::GetKeyFrameIndexAtTime(float Time, bool bClampToEnds) const {
+    if (Time < 0.0f && !bClampToEnds) {
+        return INDEX_NONE;
+    }
+
+    if (FramesPerSecond > 0.0f) {
+        return FMath::Min(FMath::FloorToInt32(Time / FramesPerSecond), GetNumKeyFrames() - 1);
+    }
+    
+    return GetNumKeyFrames() > 0 ? 0 : INDEX_NONE;
+}
+
+FSimpleFlipbookKeyFrame USimpleFlipbook::GetKeyFrameChecked(int32 Index) const {
+    check(IsValidKeyFrameIndex(Index))
+    return { .Row = Index / Columns, .Column = Index % Columns };
+}
+
+bool USimpleFlipbook::IsValidKeyFrameIndex(int32 Index) const {
+    return Index < 0 || Index >= GetNumKeyFrames();
+}
+
+FBoxSphereBounds USimpleFlipbook::GetRenderBounds() const {
+    return IsValid(ReferenceSprite) ? ReferenceSprite->GetRenderBounds() : FBoxSphereBounds(FBox(ForceInit));
+}
+
+#if WITH_EDITOR
+void USimpleFlipbook::PostEditChangeProperty(FPropertyChangedEvent &PropertyChangedEvent) {
+    if (FramesPerSecond < 0.0f) {
+        FramesPerSecond = 0.0f;
+    }
+    
+    UObject::PostEditChangeProperty(PropertyChangedEvent);
+}
+#endif
--- Plugins/Simple2D/Source/Simple2D/Public/SimpleFlipbook/Assets/SimpleFlipbook.h
+++ Plugins/Simple2D/Source/Simple2D/Public/SimpleFlipbook/Assets/SimpleFlipbook.h
@@ -3,7 +3,22 @@
 #pragma once
 
 #include "UObject/Object.h"
+#include "SpriteEditorOnlyTypes.h"
 #include "SimpleFlipbook.generated.h"
+
+class UPaperSprite;
+
+USTRUCT()
+struct FSimpleFlipbookKeyFrame {
+    GENERATED_BODY()
+
+    UPROPERTY()
+    int32 Row = 0;
+
+    UPROPERTY()
+    int32 Column = 0;
+    
+};
 
 /**
  * 
@@ -21,8 +36,8 @@ public:
     }
 
     UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
-    int32 GetTotalFrames() const {
-        return Columns;
+    int32 GetNumFrames() const {
+        return TotalFrames;
     }
 
     UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
@@ -40,28 +55,58 @@ public:
         return FramesPerSecond;
     }
 
+    UFUNCTION(BlueprintCallable, Category="Sprite")
+    int32 GetNumKeyFrames() const {
+        return GetNumFrames();
+    }
+
+    UFUNCTION(BlueprintCallable, Category="Sprite")
+    float GetTotalDuration() const;
+
+    UFUNCTION(BlueprintCallable, Category="Sprite")
+    int32 GetKeyFrameIndexAtTime(float Time, bool bClampToEnds = false) const;
+
+    FSimpleFlipbookKeyFrame GetKeyFrameChecked(int32 Index) const;
+
+    UFUNCTION(BlueprintCallable, Category="Sprite")
+    bool IsValidKeyFrameIndex(int32 Index) const;
+
+    FBoxSphereBounds GetRenderBounds() const;
+
     UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
     UMaterialInterface* GetDefaultMaterial() const {
         return DefaultMaterial;
     }
 
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    UPaperSprite* GetReferenceSprite() const {
+        return ReferenceSprite;
+    }
+
+#if WITH_EDITOR
+    void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
 private:
     UPROPERTY(EditAnywhere, BlueprintGetter = GetSourceTexture, Category = Sprite)
     TObjectPtr<UTexture2D> SourceTexture;
 
-    UPROPERTY(EditAnywhere, BlueprintGetter = GetTotalFrames, Category = Sprite)
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetNumFrames, Category = Sprite, meta = (ClamMin = 1, UIMin = 1))
     int32 TotalFrames = 1;
     
-    UPROPERTY(EditAnywhere, BlueprintGetter = GetRows, Category = Sprite)
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetRows, Category = Sprite, meta = (ClamMin = 1, UIMin = 1))
     int32 Rows = 1;
 
-    UPROPERTY(EditAnywhere, BlueprintGetter = GetColumns, Category = Sprite)
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetColumns, Category = Sprite, meta = (ClamMin = 1, UIMin = 1))
     int32 Columns = 1;
 
-    UPROPERTY(EditAnywhere, BlueprintGetter = GetFramesPerSecond, Category = Sprite)
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetFramesPerSecond, Category = Sprite, meta=(ClampMin=0, ClampMax=1000))
     float FramesPerSecond = 15.f;
     
     UPROPERTY(EditAnywhere, BlueprintGetter = GetDefaultMaterial, Category = Sprite)
     TObjectPtr<UMaterialInterface> DefaultMaterial;
+
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetReferenceSprite, Category = Sprite)
+    TObjectPtr<UPaperSprite> ReferenceSprite;
 
 };
