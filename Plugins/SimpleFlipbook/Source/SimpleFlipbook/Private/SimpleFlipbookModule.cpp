4ece6265a231a5832490c3942d5b964e139dd984
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-01-20 17:41:05 -0500

    Created the simple flipbook plugin

---
+++ Plugins/SimpleFlipbook/Source/SimpleFlipbook/Private/SimpleFlipbookModule.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleFlipbookModule.h"
+
+#define LOCTEXT_NAMESPACE "FSimpleFlipbookModule"
+
+void FSimpleFlipbookModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FSimpleFlipbookModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSimpleFlipbookModule, SimpleFlipbook)
