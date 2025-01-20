c60176ca661fa503f13460cf0f10f4d4411c21de
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-01-20 17:50:04 -0500

    Created the editor module for SimpleFlipbook

--- Plugins/SimpleFlipbook/Source/SimpleFlipbook/Private/SimpleFlipbookModule.cpp
+++ Plugins/SimpleFlipbook/Source/SimpleFlipbook/Private/SimpleFlipbookModule.cpp
@@ -2,8 +2,6 @@
 
 #include "SimpleFlipbookModule.h"
 
-#define LOCTEXT_NAMESPACE "FSimpleFlipbookModule"
-
 void FSimpleFlipbookModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
@@ -14,7 +12,5 @@ void FSimpleFlipbookModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
-
-#undef LOCTEXT_NAMESPACE
 	
 IMPLEMENT_MODULE(FSimpleFlipbookModule, SimpleFlipbook)
---
+++ Plugins/SimpleFlipbook/Source/SimpleFlipbookEditor/Private/SimpleFlipbookEditor.cpp
@@ -0,0 +1,13 @@
+﻿#include "SimpleFlipbookEditor.h"
+
+void FSimpleFlipbookEditorModule::StartupModule()
+{
+    
+}
+
+void FSimpleFlipbookEditorModule::ShutdownModule()
+{
+    // No special shutdown needed
+}
+    
+IMPLEMENT_MODULE(FSimpleFlipbookEditorModule, SimpleFlipbookEditor)
