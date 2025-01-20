e7266de906f0c0dea237502fca04de5f6d63ca3f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-01-20 17:48:38 -0500

    Added a basic SimpleFlipbook asset type

---
+++ Plugins/SimpleFlipbook/Source/SimpleFlipbook/Private/SimpleFlipbook/Assets/SimpleFlipbook.cpp
@@ -0,0 +1,9 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+
+#include "SimpleFlipbook/Assets/SimpleFlipbook.h"
+
+USimpleFlipbook::USimpleFlipbook() {
+    static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaskedMaterialRef(TEXT("/Paper2D/MaskedUnlitSpriteMaterial"));
+    DefaultMaterial = MaskedMaterialRef.Object;
+}
---
+++ Plugins/SimpleFlipbook/Source/SimpleFlipbook/Public/SimpleFlipbook/Assets/SimpleFlipbook.h
@@ -0,0 +1,68 @@
+﻿// "Unreal Pokémon" created by Retro & Chill.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SimpleFlipbook.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEFLIPBOOK_API USimpleFlipbook : public UObject {
+    GENERATED_BODY()
+
+public:
+    USimpleFlipbook();
+    
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    UTexture2D* GetSourceTexture() const {
+        return SourceTexture;
+    }
+
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    int32 GetTotalFrames() const {
+        return Columns;
+    }
+
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    int32 GetRows() const {
+        return Rows;
+    }
+
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    int32 GetColumns() const {
+        return Columns;
+    }
+
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    float GetFramesPerSecond() const {
+        return FramesPerSecond;
+    }
+
+    UFUNCTION(BlueprintPure, BlueprintInternalUseOnly)
+    UMaterialInterface* GetDefaultMaterial() const {
+        return DefaultMaterial;
+    }
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetSourceTexture, Category = Sprite)
+    TObjectPtr<UTexture2D> SourceTexture;
+
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetTotalFrames, Category = Sprite)
+    int32 TotalFrames = 1;
+    
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetRows, Category = Sprite)
+    int32 Rows = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetColumns, Category = Sprite)
+    int32 Columns = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetFramesPerSecond, Category = Sprite)
+    float FramesPerSecond = 15.f;
+    
+    UPROPERTY(EditAnywhere, BlueprintGetter = GetDefaultMaterial, Category = Sprite)
+    TObjectPtr<UMaterialInterface> DefaultMaterial;
+
+};
