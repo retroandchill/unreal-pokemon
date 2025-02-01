11902335c6177ed5faa52631948be175f119a0df
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-02-01 12:04:46 -0500

    Reverted to installing PaperZD in the editor

--- Plugins/External/PaperZD/Source/PaperZD/Private/PaperZDCustomVersion.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2017 ~ 2022 Critical Failure Studio Ltd. All rights reserved.
-
-#include "PaperZDCustomVersion.h"
-#include "Serialization/CustomVersion.h"
-
-const FGuid FPaperZDCustomVersion::GUID(0x11310AED, 0x2E554D61, 0xAF679AA3, 0xC5A1082A);
-
-// Register the custom version with core
-FCustomVersionRegistration GRegisterPaperZDCustomVersion(FPaperZDCustomVersion::GUID, FPaperZDCustomVersion::LatestVersion, TEXT("Paper_ZD_Version"));
--- Plugins/External/PaperZD/Source/PaperZDEditor/Private/Graphs/PaperZDAnimGraphSchema_Transitional.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 ~ 2022 Critical Failure Studio Ltd. All rights reserved.
-
-#include "Graphs/PaperZDAnimGraphSchema_Transitional.h"
-
-#if ZD_VERSION_INLINED_CPP_SUPPORT
-#include UE_INLINE_GENERATED_CPP_BY_NAME(PaperZDAnimGraphSchema_Transitional)
-#endif
