7192425b50d3156f6a1f22c8241f741b41963f9d
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-02-01 22:26:31 -0500

    Changed the comparison slightly

--- Plugins/DayNightCycle/Source/DayNightCycleTests/Private/Clock/TestRealTimeClock.cpp
+++ Plugins/DayNightCycle/Source/DayNightCycleTests/Private/Clock/TestRealTimeClock.cpp
@@ -1,12 +1,13 @@
-﻿#include "Asserts.h"
-#include "Clock/RealTimeClock.h"
+﻿#include "Clock/RealTimeClock.h"
 #include "Misc/AutomationTest.h"
+#include "Misc/LowLevelTestAdapter.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(TestRealTimeClock, "Unit Tests.Clock.TestRealTimeClock",
                                  EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
 
 bool TestRealTimeClock::RunTest(const FString &Parameters) {
     auto Clock = NewObject<URealTimeClock>();
-    UE_ASSERT_EQUAL(FDateTime::Now(), IGameClock::Execute_GetCurrentTime(Clock));
+    auto Now = FDateTime::Now();
+    CHECK(Now <= IGameClock::Execute_GetCurrentTime(Clock));
     return true;
 }
