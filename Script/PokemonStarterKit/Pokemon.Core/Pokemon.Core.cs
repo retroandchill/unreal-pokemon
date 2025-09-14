b7cbcdcc3282fa5bb2f3366741fd511cde80026a
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-09-14 13:22:03 -0400

    Added the service injections for the Exp growth

--- Script/PokemonStarterKit/Pokemon.Core/Pokemon.Core.cs
+++ Script/PokemonStarterKit/Pokemon.Core/Pokemon.Core.cs
@@ -1,10 +1,35 @@
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Pokemon.Core.Services.Exp;
+using UnrealInject;
 using UnrealSharp.Engine.Core.Modules;
 
 namespace Pokemon.Core;
 
+[UsedImplicitly]
 public class FPokemonCoreModule : IModuleInterface
 {
-    public void StartupModule() { }
+    public void StartupModule()
+    {
+        FUnrealInjectModule.Instance.ConfigureServices(services =>
+        {
+            services
+                .AddSingleton<IExpGrowthFormula, ErraticExpGrowthFormula>()
+                .AddSingleton<IExpGrowthFormula, FastExpGrowthFormula>()
+                .AddSingleton<IExpGrowthFormula, FluctuatingExpGrowthFormula>()
+                .AddSingleton<IExpGrowthFormula, MediumExpGrowthFormula>()
+                .AddSingleton<IExpGrowthFormula, ParabolicExpGrowthFormula>()
+                .AddSingleton<IExpGrowthFormula, SlowExpGrowthFormula>()
+                .AddSingleton<ExpGrowthFormulaProvider>();
+        });
+    }
 
-    public void ShutdownModule() { }
+    public void ShutdownModule()
+    {
+        FUnrealInjectModule.Instance.ConfigureServices(services =>
+        {
+            services.RemoveAll<IExpGrowthFormula>().RemoveAll<ExpGrowthFormulaProvider>();
+        });
+    }
 }
